c832c2f786299599fad92317877d1ccd755ce849
PierreLeidbring <pierre.leidbring@gmail.com>
2017-07-16 02:01:59 +0200

    'week card' mock up

--- mah_kronox_flutter/lib/SchedulePage.dart
+++ mah_kronox_flutter/lib/SchedulePage.dart
@@ -10,7 +10,7 @@ class SchedulePage extends StatefulWidget {
   _SchedulePageState createState() => new _SchedulePageState();
 }
 
-class _SchedulePageState extends State<SchedulePage>{
+class _SchedulePageState extends State<SchedulePage> {
 
 
   @override
@@ -18,28 +18,97 @@ class _SchedulePageState extends State<SchedulePage>{
     return new Scaffold(
         appBar: new AppBar(
             title: new Text(widget.title),
-            actions: <Widget> [
+            actions: <Widget>[
               new IconButton(
-                icon: new Icon(Icons.settings),
-                tooltip: "Test button to show another view",
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/settings');
-                },
-              )],
-        ),
+                  icon: new Icon(Icons.settings),
+                  tooltip: "Test button to show another view",
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/settings');
+                  },
+                  )
+            ],
+            ),
         body: new Column(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               new Card(
-                  child: new Text("First post")
-              ),
-              new Card(
-                  child: new Text("second post")
+                  child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        new Card(
+                            color: Colors.deepPurple,
+                            elevation: 3.0,
+                            child: new Text(
+                                "Vecka",
+                                textScaleFactor: 2.5,
+                                textAlign: TextAlign.center,
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white70
+                                )
+                            ),
+                            ),
+                        new Row(
+                            children: <Widget>[
+                              new Text(
+                                  "måndag: 56 september",
+                                  textAlign: TextAlign.left,
+                                  )
+                            ],
+                        ),
+                        new Card(
+                            elevation: 2.0,
+                            child: new Row(
+                                children: <Widget>[
+                                  new Text(
+                                      "tid",
+                                      textAlign: TextAlign.left,
+                                  ),
+                                  new Text(
+                                      "Kurs",
+                                      textAlign: TextAlign.right,
+                                  )
+                                ],
+                            )
+                        ),
+                        new Card(
+                            elevation: 2.0,
+                            child: new Row(
+                                children: <Widget>[
+                                  new Text(
+                                      "tid",
+                                      textAlign: TextAlign.left,
+                                  ),
+                                  new Text(
+                                      "Kurs",
+                                      textAlign: TextAlign.right,
+                                  )
+                                ],
+                            )
+                        ),
+                        new Card(
+                            elevation: 2.0,
+                            child: new Row(
+                                children: <Widget>[
+                                  new Text(
+                                      "tid",
+                                      textAlign: TextAlign.left,
+                                  ),
+                                  new Text(
+                                      "Kurs",
+                                      textAlign: TextAlign.right,
+                                  )
+                                ],
+                            )
+                        )
+                      ],
+                      )
+
               )
             ],
-        )
+            )
     );
   }
 }
